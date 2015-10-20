7bbb21e1e288f7df770a4ca57dbd0cda5c76fcf6
rajeshponnala <rajesh.ponnala@technoidentity.com>
2015-10-20 15:53:31 +0530

    add yesql dependency;main entry

--- project.clj
+++ project.clj
@@ -5,21 +5,23 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                  [compojure "1.3.4"]
-                   [hiccup "1.0.2"]
-                  [migratus "0.8.6"]
-                  [org.postgresql/postgresql "9.4-1203-jdbc42"]
-                  [org.clojure/java.jdbc "0.4.2"]
-                  [ring/ring-defaults "0.1.2"]
-                  [ring/ring-jetty-adapter "1.4.0"]
-                  [korma "0.4.0"]
-                  [ring/ring-json "0.4.0"]
-                  [org.clojure/clojurescript "0.0-2843"]
-                  [secretary "1.2.3"]
-                  [reagent "0.5.1"]]
+                 [compojure "1.3.4"]
+                 [hiccup "1.0.2"]
+                 [migratus "0.8.6"]
+                 [org.postgresql/postgresql "9.4-1203-jdbc42"]
+                 [org.clojure/java.jdbc "0.4.2"]
+                 [ring/ring-defaults "0.1.2"]
+                 [ring/ring-jetty-adapter "1.4.0"]
+                 [korma "0.4.0"]
+                 [ring/ring-json "0.4.0"]
+                 [org.clojure/clojurescript "0.0-2843"]
+                 [secretary "1.2.3"]
+                 [reagent "0.5.1"]
+                 [yesql "0.5.1"]]
 
   :plugins [[lein-ring "0.8.13"]
             [migratus-lein "0.1.7"]]
+  :main bookmarks.core
   :ring {:handler bookmarks.handler/app}
   :migratus {:store :database
              :migration-dir "migrations/"

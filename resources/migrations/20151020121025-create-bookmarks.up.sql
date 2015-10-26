aab3806a1bc76e301e32ea5c61391301b6c1fff2
abelia <inturi99@gmail.com>
2015-10-26 11:01:57 +0530

    update datatype character to text

--- resources/migrations/20151020121025-create-bookmarks.up.sql
+++ resources/migrations/20151020121025-create-bookmarks.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS  bookmarks
 (
 id serial NOT NULL  PRIMARY KEY,
-title character(100) NOT NULL,
-url character(100) NOT NULL,
+title text  NOT NULL,
+url text  NOT NULL,
 description text NOT NULL
 );
--- resources/migrations/20151020122008-create-tags.up.sql
+++ resources/migrations/20151020122008-create-tags.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS  tags
 (
 id serial NOT NULL  PRIMARY KEY,
-tagname character(100) NOT NULL
+tagname text  NOT NULL
 );

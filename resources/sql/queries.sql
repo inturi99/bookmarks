894141da0106fc646f23abe2dfd116be6a400b2e
rajeshponnala <rajesh.ponnala@technoidentity.com>
2015-10-29 13:46:16 +0530

    write queries for tags retrieval,creating tag,creating book-mark-tag

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -11,4 +11,20 @@ WHERE title = :title
 InSERT INTO bookmarks
     (title,url,description)
     VALUES (:title,:url,:description)
-    RETURNING title,url,description
+    RETURNING id,title,url,description
+
+-- name: get-tag-by-name
+SELECT * from tags
+WHERE tagname = :tagname
+
+-- name: create-tag
+INSERT INTO tags
+    (tagname)
+    VALUES(:tagname)
+    RETURNING id
+
+--name: create-bookmark-tag
+INSERT INTO bookmarks_tags
+    (bookmarks_id,tags_id)
+    VALUES (:bookmarkid,:tagid)
+    RETURNING bookmarks_id,tags_id
